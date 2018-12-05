2a6ed6552db524ad713288994157fb722d6f814d
MinyingHe <43507049+MinyingHe@users.noreply.github.com>
2018-12-04 22:43:34 -0500

    Create ScannerDemo.java

---
+++ ScannerDemo.java
@@ -0,0 +1,26 @@
+public class ScannerDemo {
+
+	private static String file1 = "C:\\Users\\xxlal\\eclipse-workspace\\ParserScannerOriginal\\src\\tester.txt";
+	private static int counter = 1;
+
+	public static void main(String args[]) {
+
+		TokenStream ts = new TokenStream(file1);
+		Token tok=new Token();
+
+		System.out.println(file1);
+
+		while (!ts.isEndofFile()) {
+			tok=ts.nextToken();
+			if(tok.getType().equals("Comment")) {
+				continue;
+			}
+			if(tok.getType().equals("Other")) {
+				System.out.println("Value: "+tok.getValue()+"  --> Lexical Error");
+				continue;
+			}
+			System.out.println("Token "+counter+" -->  Value: "+tok.getValue()+"   Type: "+tok.getType());
+			counter++;
+		}
+	}
+}
